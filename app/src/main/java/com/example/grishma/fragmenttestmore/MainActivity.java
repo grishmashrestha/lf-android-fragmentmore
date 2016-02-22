c91c6a8c3951170e677dee22b2c45f196db18779
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-02-22 09:30:21 +0545

    used fragment to change the color of below the menu

--- app/src/main/java/com/example/grishma/fragmenttestmore/MainActivity.java
+++ app/src/main/java/com/example/grishma/fragmenttestmore/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.grishma.fragmenttestmore;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import timber.log.Timber;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Timber.i("log log log, chop chop chop");
     }
 
     @Override
@@ -42,5 +40,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onPressRed(View view) {
+        Toast.makeText(MainActivity.this, "Red was pressed", Toast.LENGTH_SHORT).show();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentA fragmentA = new FragmentA();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.container, fragmentA, "FragmentA");
+        fragmentTransaction.commit();
+    }
+
+    public void onPressBlue(View view) {
+        Toast.makeText(MainActivity.this, "Blue was pressed", Toast.LENGTH_SHORT).show();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentB fragmentB = new FragmentB();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.container, fragmentB, "FragmentB");
+        fragmentTransaction.commit();
+    }
+
+    public void onPressGreen(View view) {
+        Toast.makeText(MainActivity.this, "Green was pressed", Toast.LENGTH_SHORT).show();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentC fragmentC = new FragmentC();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.container, fragmentC, "FragmentC");
+        fragmentTransaction.commit();
     }
 }
